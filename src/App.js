3c9cc2148b1ecbbaba14d88ec6b76c5f6b7f2e98
sachin pandey <sachingurh@gmail.com>
2023-04-01 09:08:36 +0530

    mas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { Element } from "react-scroll";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Skills from "./components/Skills";
@@ -7,14 +9,30 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
+  const [activeSection, setActiveSection] = useState("home");
+
+  const handleNavItemClick = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <div className="">
-      <NavBar />
-      <Home />
-      <About /> 
-      <Portfolio />
-      <Skills/>
-      <Contact />
+      <NavBar onNavItemClick={handleNavItemClick} />
+      <Element name="home">
+        <Home />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
+      <Element name="portfolio">
+        <Portfolio />
+      </Element>
+      <Element name="skills">
+        <Skills />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
       <SocialLinks />
     </div>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,37 +2,45 @@ import React from "react";
 
 const About = () => {
   return (
+   <div className="bg-gradient-to-b from-gray-800 to-black text-white">
+   <div name="about" className=" pb-20 ">
+
+   </div>
     <div
-      name="about"
-      className="w-full h-2/3 bg-gradient-to-b from-gray-800 to-black text-white md:pt-0 pt-12"
-    >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full ">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
-          </p>
-        </div>
-        <p className="text-xl md:mt-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          qui fugit numquam! Cum vitae temporibus molestiae dicta illum
-          laboriosam similique at mollitia nihil iusto repellat numquam eos illo
-          perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
-          maiores. Voluptatum quisquam dignissimos tempore asperiores sit
-          ratione officiis officia alias vitae.
-        </p>
-
-        <br />
-
-        <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          qui fugit numquam! Cum vitae temporibus molestiae dicta illum
-          laboriosam similique at mollitia nihil iusto repellat numquam eos illo
-          perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
-          maiores. Voluptatum quisquam dignissimos tempore asperiores sit
-          ratione officiis officia alias vitae.
+    
+    
+    className="w-full h-2/3 md:pt-0 pt-12"
+  >
+    
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full ">
+      <div className="pb-8">
+        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          About
         </p>
       </div>
+      <p className="text-xl md:mt-10 text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        qui fugit numquam! Cum vitae temporibus molestiae dicta illum
+        laboriosam similique at mollitia nihil iusto repellat numquam eos illo
+        perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
+        maiores. Voluptatum quisquam dignissimos tempore asperiores sit
+        ratione officiis officia alias vitae.
+      </p>
+
+      <br />
+
+      <p className="text-xl text-justify">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        qui fugit numquam! Cum vitae temporibus molestiae dicta illum
+        laboriosam similique at mollitia nihil iusto repellat numquam eos illo
+        perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
+        maiores. Voluptatum quisquam dignissimos tempore asperiores sit
+        ratione officiis officia alias vitae.
+      </p>
     </div>
+  </div>
+   </div> 
+   
   );
 };
 
